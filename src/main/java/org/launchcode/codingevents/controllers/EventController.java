c8ec9d0508f95f1a00ff7f279cf19e122704aa2d
rockit-man <demetrius.esq@gmail.com>
2021-05-27 23:58:23 -0500

    exercise complete w/o bonus mission

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -18,8 +18,8 @@ public class EventController {
 //    private static List<String> events = new ArrayList<>();
     private static HashMap<String, String> events = new HashMap<>();
 
-//    @GetMapping
-//    public String displayAllEvents(Model model) {
+    @GetMapping
+    public String displayAllEvents(Model model) {
 //        List<String> events = new ArrayList<>();
 //        events.add("Code With Pride");
 //        events.add("Strange Loops");
@@ -27,7 +27,14 @@ public class EventController {
 //        events.add("SpringOne Platform");
 //        model.addAttribute("events", events);
 //        return "events/index";
-//    }
+
+        events.put("Strangeloop", "A big ol' coding event in STL at the Stiefel Theatre.");
+        events.put("Battle of the Bagels", "Hold the lox.");
+        events.put("Meshuggah", "They're still open?");
+        model.addAttribute("events", events);
+        return "events/index";
+
+    }
 
     // lives at /events/create
     @GetMapping("create")
@@ -37,8 +44,8 @@ public class EventController {
 
     // lives at /events/create
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName) {
-        events.add(eventName);
+    public String createEvent(@RequestParam String eventName, @RequestParam String eventDescription) {
+        events.put(eventName, eventDescription);
         return "redirect:";
     }
 
