952151a8002b89ec91278b07aed490ce68821e04
rockit-man <demetrius.esq@gmail.com>
2021-06-05 13:38:49 -0500

    Complete but edit path needs to be fixed

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,52 +1,70 @@
 package org.launchcode.codingevents.controllers;
 
+import org.launchcode.codingevents.data.EventData;
+import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("events")
 public class EventController {
 
-//    private static List<String> events = new ArrayList<>();
-    private static HashMap<String, String> events = new HashMap<>();
-
     @GetMapping
     public String displayAllEvents(Model model) {
-//        List<String> events = new ArrayList<>();
-//        events.add("Code With Pride");
-//        events.add("Strange Loops");
-//        events.add("Apple WWDC");
-//        events.add("SpringOne Platform");
-//        model.addAttribute("events", events);
-//        return "events/index";
-
-        events.put("Strangeloop", "A big ol' coding event in STL at the Stiefel Theatre.");
-        events.put("Battle of the Bagels", "Hold the lox.");
-        events.put("Meshuggah", "They're still open?");
-        model.addAttribute("events", events);
+        model.addAttribute("title", "All Events");
+        model.addAttribute("events", EventData.getAll());
         return "events/index";
-
     }
 
     // lives at /events/create
     @GetMapping("create")
-    public String renderCreateEventForm() {
+    public String renderCreateEventForm(Model model) {
+//        model.addAttribute("title", "Create Event");
         return "events/create";
     }
 
     // lives at /events/create
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName, @RequestParam String eventDescription) {
-        events.put(eventName, eventDescription);
+    public String createEventForm(@ModelAttribute Event newEvent) {
+        EventData.addEvent(newEvent);
         return "redirect:";
     }
 
+    // displays form
+    @GetMapping("delete")
+    public String displayDeleteEventForm(Model model) {
+        model.addAttribute("title", "Delete Event");
+        model.addAttribute("events", EventData.getAll());
+        return "events/delete";
+    }
+
+    @PostMapping("delete")
+    public String processDeleteEventsForm(@RequestParam(required = false) int[] eventIds) {
+
+        if (eventIds != null) {
+            for (int id : eventIds) {
+                EventData.removeEvent(id);
+            }
+        }
+        return "redirect:";
+    }
+
+    @GetMapping("edit/{eventId}")
+    public String displayEditForm(Model model, @PathVariable int eventId) {
+        Event selectedEvent = EventData.getById(eventId);
+        model.addAttribute("selectedEvent", selectedEvent);
+        String title = "Edit Event" + selectedEvent.getName() + "(id" + selectedEvent.getId() + ")";
+        model.addAttribute("title", title);
+        return "events/edit";
+    }
+
+    @PostMapping("edit/{eventId}")
+//    @RequestMapping(value = "eventsedit", method = {RequestMethod.GET, RequestMethod.POST})
+    public String processEditForm(int eventId, String name, String description) {
+        Event selectedEvent = EventData.getById(eventId);
+        selectedEvent.setName(name);
+        selectedEvent.setDescription(description);
+        return "redirect:";
+    }
 }
